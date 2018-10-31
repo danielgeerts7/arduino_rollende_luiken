48cadb7e2648d88afc7bb44e44cb7eed042aedb8
danielgeerts7 <daniel.geerts@live.nl>
2018-10-31 12:00:16 +0100

    Ultrasoonmeter not working yet, the LEDs are.

--- arduino_rollende_luiken/arduino_rollende_luiken.c
+++ arduino_rollende_luiken/arduino_rollende_luiken.c
@@ -7,6 +7,7 @@
 
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 const int trigPin = 2; // Trigger		PD2
 const int echoPin = 3; // Echo			PD3
@@ -18,8 +19,23 @@ int main(void)
 {
 	DDRD = 0b01110100; // Set Trigger as OUTPUT and ECHO as INPUT && set all LEDs as OUTPUT
 	
+	long duration;
+	int distance;
+	
     while(1)
     {
-        //TODO:: Please write your application code
+		duration = PORTD3;
+		distance = duration*0.034/2;
+		
+		if (distance < 10) {
+			//PORTD = PORTD & 0b01000000;
+			PORTD ^= (1<<6);
+		} else if (distance < 20) {
+			//PORTD = PORTD & 0b00100000;
+			PORTD ^= (1<<5);
+		} else {
+			//PORTD = PORTD & 0b00010000;
+			PORTD ^= (1<<4);
+		}
     }
 }
