02a8e631269b66d05f4590567306f1fbe552b7b9
Florian Molenaars <f.molenaars@hotmail.com>
2018-11-06 13:43:04 +0100

    added timer0, schedular, switchmode and light sensor.

--- arduino_rollende_luiken/main.c
+++ arduino_rollende_luiken/main.c
@@ -2,7 +2,7 @@
  * arduino_rollende_luiken.c
  *
  * Created: 30-Oct-18 12:21:19
- *  Author: Daniel Geerts
+ *  Author: Daniel Geerts && Florian Molenaars
  */ 
 
 
@@ -10,15 +10,28 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "display.h"
+#include "schedular.h"
+
 
 const int trigPin = 0;		// Trigger		PD0
 const int echoPin = 3;		// Echo			PD3
 const int RED_LED = 4;		// Red LED		PD4
 const int YELLOW_LED = 5;	// Yellow LED	PD5
 const int GREEN_LED = 6;	// Green LED	PD6
+const int LightSensor = 0;  // lightsensor  PA0
 
 volatile uint16_t gv_counter; // 16 bit counter value
 volatile uint8_t gv_echo; // a flag
+volatile uint16_t light_sensitivity = -1; //temperature
+volatile uint16_t distance; //distance of roller shutter
+uint8_t distant_max = 65;	//max distant roller shutter
+uint8_t distant_min = 5;	//min distant roller shutter
+uint8_t light_min = 40;		//min light intensity
+uint8_t light_max = 130;	//max light intensity
+
+// setting up mode for arduino
+typedef enum{ROLLING_UP= 0, ROLLING_DOWN = 1, WAITING = 2} mode_t;
+	mode_t mode = WAITING;
 
 void init_ports(void)
 {
@@ -33,11 +46,10 @@ void init_ports(void)
 
 void init_timer(void)
 {
-	// prescaling : max time = 2^16/16E6 = 4.1 ms, 4.1 >> 2.3, so no prescaling required
-	// normal mode, no prescale, stop timer
 	TCCR1A = 0;
 	TCCR1B = 0;
 }
+
 
 void init_ext_int(void)
 {
@@ -53,44 +65,160 @@ uint16_t calc_cm(uint16_t counter)
 	uint16_t microSec = counter / 16;
 	return (microSec/58.2);
 }
+void init_adc()
+{
+	// ref=Vcc, left adjust the result (8 bit resolution),
+	// select channel 0 (PC0 = input)
+	ADMUX = (1<<REFS0)|(1<<ADLAR);
+	// enable the ADC & prescale = 128
+	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+
+
+uint8_t get_adc_value()
+{
+	ADCSRA |= (1<<ADSC); // start conversion
+	loop_until_bit_is_clear(ADCSRA, ADSC);
+	return ADCH; // 8-bit resolution, left adjusted
+}
+
+
+void check_light()
+{
+	static int send_info = 0; // 0 - false, 1 - true
+	uint8_t temp = light_sensitivity;
+	light_sensitivity = get_adc_value();
+	_delay_ms(100);
+	
+	if (light_sensitivity > 5)
+	{
+		//if (temp > 0) {
+			//light_sensitivity = (light_sensitivity + temp) / 2;
+		//}
+		if (light_sensitivity >= light_max) {
+			mode = ROLLING_DOWN;
+		} else if (light_sensitivity <= light_min) {
+			mode = ROLLING_UP;
+		} else {
+			mode = WAITING;
+		}
+		
+	}
+		if (send_info == 1)
+		{
+			// Send all information to Python 'De Centrale'
+			send_info = 0;
+		} 
+		else 
+		{
+			send_info = 1;
+		}					
+}
+
+
+void roll_down(void)
+{
+	while(distance <= distant_max){
+		PORTD ^= (1 << YELLOW_LED);;
+		
+		if(distance >= distant_max || distance <= distant_min)
+		{
+			PORTD &= ~(1 << YELLOW_LED);
+		}
+		
+		show_distance(distance);
+		distance += 1;
+		_delay_ms(1000);
+	}
+	
+	_delay_ms(5000);
+	mode = WAITING;
+}	
+
+
+void roll_up(void)
+{
+	while(distance >= distant_min){
+		PORTD |= (1 << YELLOW_LED);
+		
+		if(distance >= distant_max || distance <= distant_min)
+		{
+			PORTD &= ~(1 << YELLOW_LED);
+		}
+		
+		show_distance(distance);
+		distance -= 1;
+		_delay_ms(1000);
+	}
+	
+	_delay_ms(5000);
+	mode = WAITING;
+}
+
+void calc_distance() 
+{
+	gv_echo = BEGIN;	// Set gv_echo to BEGIN
+	PORTD |= _BV(trigPin);	// Set trigPin to 1 -> send pulse
+	_delay_us(12);			// Wait for pulse to complete
+	PORTD &= ~(1<<trigPin);		// Clear PORTD (trigPin & LEDs)
+	PORTD &= ~(1<<echoPin);
+	_delay_ms(30);			// Wait to make sure the signal of the pulse has been returned to echo
+	
+	distance = calc_cm(gv_counter);
+	show_distance(distance);
+}
 
 
 int main(void)
 {
+	init_ext_int();	 // Turn required external interrupts on
 	init_timer();	 // Enable timer interrupts
 	init_ports();    // Enable ports
-	init_ext_int();	 // Turn required interrupts on
 	reset_display(); // Clear display
+	init_adc();		 // Enable analog
+	SCH_Init_T0();	 // Enable schedualr
+	int t1 = SCH_Add_Task(check_light,0,100); // check light intensity
 	sei();			 // Set interrupt flag
-	
 	_delay_ms(50);	// Make sure everything is initialized
 	
-    while(1)
-    {
-		gv_echo = BEGIN;		// Set gv_echo to BEGIN
-		PORTD |= _BV(trigPin);	// Set trigPin to 1 -> send pulse
-		_delay_us(12);			// Wait for pulse to complete
-		PORTD = 0x00;			// Clear PORTD (trigPin & LEDs)
+	while(1)
+	{
+		SCH_Dispatch_Tasks();
+		PORTD &= ~(1 << RED_LED);
+		PORTD &= ~(1 << GREEN_LED);
 		
-		_delay_ms(30);			// Wait to make sure the signal of the pulse has been returned to echo
-		
-		uint16_t distance = calc_cm(gv_counter);
-		show_distance(distance);
-		
-		if (distance < 10) {
-			PORTD |= (1<<GREEN_LED);		// Set green LED to 1
-		} else if (distance < 30) {
-			PORTD |= (1<<YELLOW_LED);		// Set yellow LED to 1
-		} else {
-			PORTD |= (1<<RED_LED);			// Set red LED to 1
+		switch(mode)
+		{
+			case ROLLING_DOWN:
+				roll_down();
+				break;
+			case ROLLING_UP:
+				roll_up();
+				break;
+			case WAITING:
+				calc_distance();
+				break;
 		}
 		
+		if (distance > distant_max)
+		{
+			PORTD |= (1<<GREEN_LED);		// Set green LED to 1
+		}
+		else if (distance < distant_min) 
+		{
+			PORTD |= (1<<RED_LED); // Set yellow LED to 1
+		}	
+
 		_delay_ms(500);
-    }
+		
+	}	
+	SCH_Delete_Task(t1);
+	cli();
 }
 
-ISR (INT1_vect)
-{
+
+ISR(INT1_vect)
+{	
 	if (gv_echo == BEGIN) {
 		TCNT1 = 0;
 		TCCR1B = _BV(CS10);
@@ -100,3 +228,5 @@ ISR (INT1_vect)
 		gv_counter = TCNT1;
 	}
 }
+
+
---
+++ arduino_rollende_luiken/schedular.c
@@ -0,0 +1,122 @@
+
+/*
+* schedular.s
+*
+* Created: 5-11-2018 11:49:09
+*  Author: 
+*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "schedular.h"
+// The array of tasks
+sTask SCH_tasks_G[SCH_MAX_TASKS];
+
+void SCH_Dispatch_Tasks(void)
+{
+	unsigned char Index;
+
+	// Dispatches (runs) the next task (if one is ready)
+	for(Index = 0; Index < SCH_MAX_TASKS; Index++)
+	{
+		if((SCH_tasks_G[Index].RunMe > 0) && (SCH_tasks_G[Index].pTask != 0))
+		{
+			(*SCH_tasks_G[Index].pTask)();  // Run the task
+			SCH_tasks_G[Index].RunMe -= 1;   // Reset / reduce RunMe flag
+
+			// Periodic tasks will automatically run again
+			// - if this is a 'one shot' task, remove it from the array
+			if(SCH_tasks_G[Index].Period == 0)
+			{
+				SCH_Delete_Task(Index);
+			}
+		}
+	}
+}
+
+unsigned char SCH_Add_Task(void (*pFunction)(), const unsigned int DELAY, const unsigned int PERIOD)
+{
+	unsigned char Index = 0;
+
+	// First find a gap in the array (if there is one)
+	while((SCH_tasks_G[Index].pTask != 0) && (Index < SCH_MAX_TASKS))
+	{
+		Index++;
+	}
+
+	// Have we reached the end of the list?
+	if(Index == SCH_MAX_TASKS)
+	{
+		// Task list is full, return an error code
+		return SCH_MAX_TASKS;
+	}
+
+	// If we're here, there is a space in the task array
+	SCH_tasks_G[Index].pTask = pFunction;
+	SCH_tasks_G[Index].Delay =DELAY;
+	SCH_tasks_G[Index].Period = PERIOD;
+	SCH_tasks_G[Index].RunMe = 0;
+
+	// return position of task (to allow later deletion)
+	return Index;
+}
+
+
+unsigned char SCH_Delete_Task(const unsigned char TASK_INDEX)
+{
+	// Return_code can be used for error reporting, NOT USED HERE THOUGH!
+	unsigned char Return_code = 0;
+
+	SCH_tasks_G[TASK_INDEX].pTask = 0;
+	SCH_tasks_G[TASK_INDEX].Delay = 0;
+	SCH_tasks_G[TASK_INDEX].Period = 0;
+	SCH_tasks_G[TASK_INDEX].RunMe = 0;
+
+	return Return_code;
+}
+
+void SCH_Init_T0(void)
+{
+	unsigned char i;
+
+	for(i = 0; i < SCH_MAX_TASKS; i++)
+	{
+		SCH_Delete_Task(i);
+	}
+	
+	// Set up Timer 0
+	// prescale op 1024
+	TCCR0B = (1 << CS02) | (1 << CS00);
+	TCCR0A = (1 << WGM01);
+	TIMSK0 = (1 << OCIE0A); // Timer 0 Output Compare A Match Interrupt Enable
+	OCR0A = (uint8_t)156; // 10 mili sec = (1024/16.000.000)*156
+	
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+	unsigned char Index;
+	for(Index = 0; Index < SCH_MAX_TASKS; Index++)
+	{
+		// Check if there is a task at this location
+		if(SCH_tasks_G[Index].pTask)
+		{
+			if(SCH_tasks_G[Index].Delay == 0)
+			{
+				// The task is due to run, Inc. the 'RunMe' flag
+				SCH_tasks_G[Index].RunMe += 1;
+
+				if(SCH_tasks_G[Index].Period)
+				{
+					// Schedule periodic tasks to run again
+					SCH_tasks_G[Index].Delay = SCH_tasks_G[Index].Period;
+					SCH_tasks_G[Index].Delay -= 1;
+				}
+			}
+			else
+			{
+				// Not yet ready to run: just decrement the delay
+				SCH_tasks_G[Index].Delay -= 1;
+			}
+		}
+	}
+}
